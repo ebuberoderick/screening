5588c71746459ad1f9f83114780c6f36de2597d4
ebuberoderick <ebuberoderick2@gmail.com>
2024-01-28 12:10:57 +0000

    deployment fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
-'use client'
-import { useRouter } from "next/navigation";
+import AppLayout from '@/components/layouts/AppLayout'
+import React from 'react'
 
-export default function Home() {
-  const router = useRouter()
-  router.push("/overview")
-  return null
+function page() {
+  return (
+    <AppLayout>page</AppLayout>
+  )
 }
+
+export default page
